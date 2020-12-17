032e8237d224532844e680928443321df372ce5f
Marcogarofalo <marcogarofalo31@gmail.com>
2020-12-17 17:56:48 +0100

    C2t0 as reference paper

--- modules/measurements.cpp
+++ modules/measurements.cpp
@@ -193,15 +193,16 @@ void  compute_G2t(const Viewphi &phi, cluster::IO_params params , FILE *f_G2t ){
             double pp1=h_phip(1,t1) *h_phip(1 , tpt1);
             G2t0+=pp0;
             G2t1+=pp1; 
-            C2t0+= pp0*pp0 + pp1*pp1 + 2*pp0*pp1   ;
-            C2t1+= pp0*pp0 + pp1*pp1 + 2*pp0*pp1   ; 
+            C2t0+= pp0*pp0 + pp1*pp1 + 4*pp0*pp1
+                +2* h_phip(0,t1) *h_phip(0 , t1)* h_phip(1,tpt1) *h_phip(1 , tpt1)
+                +2* h_phip(1,t1) *h_phip(1 , t1)* h_phip(0,tpt1) *h_phip(0 , tpt1);
+            C2t1+=  pp1*pp1 ; 
         } 
         G2t0*=2.*params.data.kappa0/((double) T);
         G2t1*=2.*params.data.kappa1/((double) T);
         C2t0*=4.*params.data.kappa0*params.data.kappa0/((double) T);
         C2t1*=4.*params.data.kappa1*params.data.kappa1/((double) T);
         
-
         fprintf(f_G2t,"%d \t %.12g \t %.12g  \t %.12g \t %.12g \n",t,G2t0,G2t1,C2t0,C2t1);
     }
 
