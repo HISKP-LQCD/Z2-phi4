8158df8f24191abf0f60e4de517fcdc6a0ce02ad
Marcogarofalo <marcogarofalo31@gmail.com>
2022-02-08 19:56:53 +0100

    comments

--- modules/metropolis.cpp
+++ modules/metropolis.cpp
@@ -114,8 +114,13 @@ double metropolis_update(Viewphi& phi, cluster::IO_params params, RandPoolType& 
 
             // if spin flip phi0
             if (rgen.urand(0, 2)) {
-                // change of action   S= - 2 kappa phi \sum phi 
-                // S[new]-S[old]= - 2 kappa (-phi) \sum phi + 2 kappa phi \sum phi = 4 kappa phi \sum phi
+                // change of action   S= - 2 kappa phi[0] neighbourSum[0] + g phi[1] phi[0] neighbourSum[0]^2
+                // kinetic term: S[new]-S[old]
+                //      =- 2 kappa (-phi) neighbourSum[0] + 2 kappa phi neighbourSum[0] 
+                //      = 4 kappa phi neighbourSum[0]
+                // coupling: S[new]-S[old]
+                //      = g phi[1] (-phi[0]) neighbourSum[0]^2-g phi[1] phi[0] neighbourSum[0]^2
+                //      = -2 g phi[1] (-phi[0]) neighbourSum[0]^2
                 double dS = 4. * kappa[0] * phi(0, x) * neighbourSum[0];
                 dS -= 2 * g * neighbourSum[0] * neighbourSum[0] * phi(0, x) * phi(1, x);
                 //  accept reject step -------------------------------------
